3cfa774118a5e920e9a13ca491804eddf9ea92fa
Jan Sobielarski <jsobielarski@gmail.com>
2019-02-27 15:04:27 +0100

    User model extended + migrations, phone regex validator

--- AMS/accounts/models.py
+++ AMS/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class UserExtended(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=64, blank=True)
+    street = models.CharField(max_length=64, blank=True)
+    postal_code = models.CharField(max_length=5, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+
--- AMS/main/models.py
+++ AMS/main/models.py
@@ -1,13 +1,6 @@
 from datetime import timedelta
 
-from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
-
-
-class UserExtended(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 DURATION_TIME_CHOICES = (

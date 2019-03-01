acf548fe130cd966fbe9ea5eda073405ce999592
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-01 13:56:08 +0100

    Rest configuration

--- AMS/AMS/settings.py
+++ AMS/AMS/settings.py
@@ -122,6 +122,15 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 try:
     from AMS.local_settings import DATABASES
 except ModuleNotFoundError:
